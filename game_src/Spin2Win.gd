245ca0e2942460cddc319acbafe4b107ed38bc92
tremaines <tremaine.stroebel@gmail.com>
2023-07-15 13:48:35 +1000

    Added basic rotary scene plus some extra assets

---
+++ game_src/Spin2Win.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+var speed = 400
+var angular_speed = PI
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	rotation += angular_speed * delta
